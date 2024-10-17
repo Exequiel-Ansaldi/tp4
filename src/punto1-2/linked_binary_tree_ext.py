ab8fdcab1e9bd8e93424cab9d7ca84d4179f5d1c
Exequiel-Ansaldi <exeansaldi0@gmail.com>
2024-10-17 00:04:28 -0300

    hojas metodo(falta probar)

--- src/punto1-2/linked_binary_tree_ext.py
+++ src/punto1-2/linked_binary_tree_ext.py
@@ -1,4 +1,9 @@
 from data_structures import LinkedBinaryTree
 from linked_binary_tree_ext_abstract import LinkedBinaryTreeExtAbstract
 class LinkedBinaryTreeExt( LinkedBinaryTree,LinkedBinaryTreeExtAbstract):
-    pass
+    def hojas(self):
+        lista = []
+        for nodo in self.__iter__():
+            if nodo.children_count() == 0:
+                lista.append(nodo.element)
+        return lista
